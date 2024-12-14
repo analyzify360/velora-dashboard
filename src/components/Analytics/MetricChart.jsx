32d82b51b9c6c46fd2208211d3d42cc8723350dc
minion50000 <167047332+minion50000@users.noreply.github.com>
2024-12-14 00:57:46 -0600

    chore: remove keymetric comp

--- src/components/Analytics/MetricChart.jsx
+++ src/components/Analytics/MetricChart.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Paper, Typography } from '@mui/material';
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 import axios from 'axios';
 import {
   Chart as ChartJS,
   LineElement,
+  BarElement,
   CategoryScale,
   LinearScale,
+  LogarithmicScale,
   PointElement,
   Title,
   Tooltip,
@@ -18,8 +20,10 @@ import zoomPlugin from 'chartjs-plugin-zoom';
 
 ChartJS.register(
   LineElement,
+  BarElement,
   CategoryScale,
   LinearScale,
+  LogarithmicScale,
   PointElement,
   Title,
   Tooltip,
@@ -35,6 +39,7 @@ const MetricChart = ({ viewType, metrics, metricType, extraData}) => {
     labels: metrics.map((metric) => new Date(metric.timestamp * 1000).toLocaleTimeString('en-US', { timeZone: 'UTC' })),
     datasets: [
       {
+        type: metricType === 'volume' ? 'bar' : 'line',
         label: metricType === 'price' ? 'Price' : metricType === 'liquidity' ? 'Liquidity' : 'Volume',
         data: metricType === 'price' ? metrics.map((metric) => metric.price) : metricType === 'liquidity' ? metrics.map((metric) => metric.liquidity_token0 * extraData.token0_price + metric.liquidity_token1 * extraData.token0_price) : metrics.map((metric) => metric.volume_token0 * extraData.token0_price + metric.volume_token1 * extraData.token1_price),
         borderColor: metricType === 'price' ? 'rgba(75, 192, 192, 1)' : metricType === 'liquidity' ? 'rgba(153, 102, 255, 1)' : 'rgba(255, 159, 64, 1)',
@@ -42,19 +47,22 @@ const MetricChart = ({ viewType, metrics, metricType, extraData}) => {
         fill: false,
         pointRadius: 1,
         pointHitRadius: 5,
+        barThickness: metricType === 'volume' ? 2 : undefined,
       },
     ],
   } : {
     labels: metrics.map((metric) => new Date(metric.timestamp * 1000).toLocaleTimeString('en-US', { timeZone: 'UTC' })),
     datasets: [
       {
+        type: metricType === 'volume' ? 'bar' : 'line',
         label: metricType === 'price' ? 'Price' : metricType === 'liquidity' ? 'Liquidity' : 'Volume',
-        data: metricType === 'price' ? metrics.map((metric) => metric.close_price) : metricType === 'liquidity' ? metrics.map((metric) => metric.total_liquidity * metric.close_price) : metrics.map((metric) => metric.total_volume * metric.close_price),
+        data: metricType === 'price' ? metrics.map((metric) => metric.close_price) : metricType === 'liquidity' ? metrics.map((metric) => metric.total_liquidity * metric.close_price) : metrics.map((metric, index) => index === 0 ? 0 : (metric.total_volume - metrics[index - 1].total_volume) * metric.close_price),
         borderColor: metricType === 'price' ? 'rgba(75, 192, 192, 1)' : metricType === 'liquidity' ? 'rgba(153, 102, 255, 1)' : 'rgba(255, 159, 64, 1)',
-        backgroundColor: metricType === 'price' ? 'rgba(75, 192, 192, 0.2)' : metricType === 'liquidity' ? 'rgba(153, 102, 255, 0.2)' : 'rgba(255, 159, 64, 0.2)',
+        backgroundColor: metricType === 'price' ? 'rgba(75, 192, 192, 0.2)' : metricType === 'liquidity' ? 'rgba(153, 102, 255, 0.2)' : 'rgba(255, 159, 64, 0.7)',
         fill: false,
         pointRadius: 1,
         pointHitRadius: 5,
+        barThickness: metricType === 'volume' ? 2 : undefined,
       },
     ],
   }
@@ -87,6 +95,7 @@ const MetricChart = ({ viewType, metrics, metricType, extraData}) => {
       },
       y: {
         beginAtZero: true,
+        // type: metricType === 'volume' ? 'linear' : 'logarithmic',
       },
     },
   };
@@ -96,7 +105,11 @@ const MetricChart = ({ viewType, metrics, metricType, extraData}) => {
       <Typography variant="h5" gutterBottom>
         {chartLabel}
       </Typography>
-      <Line data={chartData} options={options} />
+      {metricType === 'volume' ? (
+        <Bar data={chartData} options={options} />
+      ) : (
+        <Line data={chartData} options={options} />
+      )}
     </Paper>
   );
 };
